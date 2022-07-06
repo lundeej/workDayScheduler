dc0569894af85eb527ad85e1405c44e3d0ffb4ed
Jesenia Lundeen <jeseniathenotary@gmail.com>
2022-07-05 22:17:41 -0700

    adding HTML template

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 // currentTime 
-var currentTime = moment().format("dddd MMMM Do YY"); 
+var currentTime = moment().format("dddd, MMMM Do YYYY"); 
 console.log(currentTime); 
+
+//currentHour 
+var currentHour = moment().format("hh"); 
+console.log(currentHour); 
 
 // currentDay Element 
 var currentDay = document.querySelector("#currentDay"); 
 currentDay.textContent = currentTime; 
 
 // timeEntry container Element 
-var timeEntryContainer = document.querySelector("#container"); 
+var timeEntryContainer = document.querySelector(".container"); 
 
 // block for each hour of the day 
-for(var i = 9; i <= 17; i++) {
+for(var i = 5; i <= 20; i++) {
     //attempt to get the saved data for the hour of the loop 
-    var data = "HELLO"; 
+    var savedData = "hour-"+i; 
+
+    var data = ""; 
+
+    //compare i to current hour to determine if hour is in the past/present/future 
 
     var template = `
         <div class="row">
@@ -21,5 +29,13 @@ for(var i = 9; i <= 17; i++) {
         <div><button data-hour="${i}">Save</button></div>
         </div>
     `; 
-    
+    //append the HTML to the page 
+   var timeEntryDiv = document.getElementById("timeEntries"); 
+   timeEntryDiv.append(template); 
 }
+
+// save data using the container 
+timeEntryContainer.on("click", "button", function() {
+    console.log("yes"); 
+
+}); 
